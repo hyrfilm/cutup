9a9277927dd81b90a355a1c37176d6f274527574
jonasholmer <jonas@mindmore.com>
2025-03-04 15:38:08 +0100

    Added library module

--- justfile
+++ justfile
@@ -9,6 +9,6 @@ test:
     (cd tests && uv run pytest)
 
 clean:
-    find . -type f -name "*.pyc" -delete && uv cache clean && rm -rf .venv
+    find . -type f -name "*.pyc" -delete && find . -type d -name "__pycache__" -delete && uv cache clean && rm -rf .venv
 
 all: format lint test
---
+++ library/__init__.py
@@ -0,0 +1,9 @@
+# the library module is intended to make it easy for scripts
+# to just import the public utility functions
+
+from src.console import log, with_spinner, indented_log
+from src.io import read_file, write_file, touch
+from src.search import search_files
+from src.insertion import split_at, split_rows_by_delimiter
+from src.prompting import prompt
+from src.env_vars import get_env_var, print_env_vars
